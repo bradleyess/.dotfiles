027fd1eff4bbe45bbd9d678f1d454ee374404e82
Bradley Shawyer <bradley@blstr.co>
2020-01-15 20:56:28 +1100

    feat(python): add helper exports for virtualenv

---
+++ zsh/config/python.zsh
@@ -0,0 +1,6 @@
+
+# Virtual Env Wrapper
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
