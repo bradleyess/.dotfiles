16819d0a6fa364082300d59f8b96869805fc991f
Bradley Shawyer <bradley@blstr.co>
2020-06-29 18:07:01 +1000

    feat(terraform): add terraform aliases

--- aliases/terraform.sh
+++ aliases/terraform.sh
@@ -1,3 +1,11 @@
+# Terraform
+alias tf="terraform"
+alias tfi="terraform init"
+alias tfp="terraform plan"
+alias tfa="terraform apply"
+
+# Terragrunt
 alias tg="terragrunt"
 alias tgp="terragrunt plan"
 alias tga="terragrunt apply"
+alias tgfu="terragrunt force-unlock"
