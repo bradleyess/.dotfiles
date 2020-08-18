c2d9f19c331b100149dbc34e159b1ddae2dbf794
Bradley Shawyer <bradley@blstr.co>
2020-08-18 14:21:45 +0930

    feat(video): add video function for creating mp4 from audio and image files w/ ffmpeg

--- functions/video.sh
+++ functions/video.sh
@@ -5,5 +5,5 @@ createVideoFromImageAndAudio()  {
     audioFile=$2
     title=$3
 
-    ffmpeg -y -i "$imageFile" -i "$audioFile" -vcodec libx264 -tune stillimage -acodec aac "$title.mp4"
+    ffmpeg -loop 1 -i "$imageFile" -i "$audioFile" -c:v libx264 -c:a aac -shortest "$title.mp4"
 }
