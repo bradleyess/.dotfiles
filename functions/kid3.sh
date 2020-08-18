58aea1cd97d033047cdce7f762d6dcee1bc062c5
Bradley Shawyer <bradley@blstr.co>
2020-08-18 14:24:37 +0930

    chore(functions): remove kid3 script as unused

--- functions/kid3.sh
+++
@@ -1,7 +0,0 @@
-
-setAiffArtworkFromDiscogs() {
-
-    # Curl Discogs with title of track.
-
-    kid3-cli -c 'select "$1"' -c 'set picture:"/path/to/coverart.jpg" "Picture Description"' -c 'save'
-}
