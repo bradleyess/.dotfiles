a7771a7d9ad6137e17d2243572a7c32ea8bbcdd4
bradleyess <bradley.shawyer@gmail.com>
2020-09-20 18:39:00 +0930

    feat(zsh): install pure theme and wait for git plugin with zinit

--- zsh/plugins/zplugin.zsh
+++ zsh/plugins/zplugin.zsh
@@ -4,3 +4,11 @@ zinit light zdharma/zsh-diff-so-fancy
 zinit light zsh-users/zsh-autosuggestions
 zinit light zdharma/fast-syntax-highlighting
 zinit light agkozak/zsh-z
+
+zinit ice pick"async.zsh" src"pure.zsh"
+zinit light sindresorhus/pure
+
+zinit wait lucid for \
+        OMZL::git.zsh \
+        atload"unalias grv" \
+        OMZP::git
