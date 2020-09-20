6b406d7081bac611f2950e73b0ca6a502b7ac934
bradleyess <bradley.shawyer@gmail.com>
2020-09-20 18:41:33 +0930

    tidy(*): remove sls autocompletes

--- zsh/config/serverless.zsh
+++
@@ -1,9 +0,0 @@
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[[ -f /Users/bx/linktr.ee/monorepo/node_modules/tabtab/.completions/serverless.zsh ]] && . /Users/bx/linktr.ee/monorepo/node_modules/tabtab/.completions/serverless.zsh
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[[ -f /Users/bx/linktr.ee/monorepo/node_modules/tabtab/.completions/sls.zsh ]] && . /Users/bx/linktr.ee/monorepo/node_modules/tabtab/.completions/sls.zsh
-# tabtab source for slss package
-# uninstall by removing these lines or running `tabtab uninstall slss`
-[[ -f /Users/bx/linktr.ee/monorepo/node_modules/tabtab/.completions/slss.zsh ]] && . /Users/bx/linktr.ee/monorepo/node_modules/tabtab/.completions/slss.zsh
