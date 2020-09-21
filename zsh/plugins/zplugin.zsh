19cadf0d348b7a9202882e17442fcb7cf1d6854f
Bradley Shawyer <bradley.shawyer@gmail.com>
2020-09-21 11:11:56 +0930

    feat(zsh): install kube-ps1 for kube zsh features

--- zsh/plugins/zplugin.zsh
+++ zsh/plugins/zplugin.zsh
@@ -9,6 +9,8 @@ zinit ice pick"async.zsh" src"pure.zsh"
 zinit light sindresorhus/pure
 
 zinit wait lucid for \
-        OMZL::git.zsh \
-        atload"unalias grv" \
-        OMZP::git
+    OMZL::git.zsh \
+    atload"unalias grv" \
+    OMZP::git
+
+zinit light jonmosco/kube-ps1
