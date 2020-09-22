d32d9e0538c56ecade1ff47756a0a57185598731
Bradley Shawyer <21098019+bradleyess@users.noreply.github.com>
2020-09-23 07:34:28 +0930

    feat(brew): install docker-compose

--- brew.sh
+++ brew.sh
@@ -21,21 +21,25 @@ apps=(
     exiftool guetzli imagemagick jpeg libpng optipng ffmpeg
 
     # Shell and CLI
-    awscli bat circleci coreutils curl diff-so-fancy fd fdupes findutils git git-crypt git-extras gnu-sed gpg grc grep
+    bat coreutils curl diff-so-fancy fd fdupes findutils git git-crypt git-extras gnu-sed gpg grc grep
     exa httpie hub jq mkcert moreutils mtr mutt nss reattach-to-user-namespace ripgrep tig tldr tmux tree wget z xsv
     adr-tools fastly/tap/fastly
-    docker docker-machine kubectl minikube helm kubectx kube-ps1 kubespy
-    terraform terragrunt tflint
-    pulumi
-    python3 pyenv
-    node go rust
-    yarn
     zsh zsh-completions zsh-syntax-highlighting zplug
 
     # Creative Coding / C++
     pkg-config gcc readline sqlite gdbm freetype
     eigen libyaml fftw libsamplerate libtag tensorflow
     cairo
+
+    # Development environment
+    awscli circleci
+    docker docker-machine docker-compose kubectl minikube helm kubectx kube-ps1 kubespy stern derailed/popeye/popeye
+    node go rust
+    postgres
+    pulumi
+    python3 pyenv
+    terraform terragrunt tflint
+    yarn
 )
 
 brew install "${apps[@]}"
