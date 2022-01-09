8ab0d686a62f1171c987371049d589b5852444ef
Bradley Shawyer <21098019+bradleyess@users.noreply.github.com>
2022-01-09 22:26:06 +1100

    brew: install fswatch

--- brew.sh
+++ brew.sh
@@ -22,9 +22,11 @@ apps=(
     blackhole-2ch blackhole-16ch
 
     # Shell and CLI
+    himalaya # Email client
+    dust     # du in Rust
     bat coreutils curl diff-so-fancy fd fdupes findutils git git-crypt git-extras gnu-sed gpg grc grep
-    exa httpie hub jq mkcert moreutils mtr mutt nss reattach-to-user-namespace ripgrep tig tldr tmux tree wget z xsv
-    adr-tools fastly/tap/fastly
+    exa httpie hub jq mkcert moreutils mtr nss reattach-to-user-namespace ripgrep tig tldr tmux tree wget z xsv
+    adr-tools fastly/tap/fastly fswatch
     zsh zsh-completions zsh-syntax-highlighting zplug
 
     # Creative Coding / C++
