b73664a5fedac454dea65e5946f5d0eea05ebeac
Bradley Shawyer <21098019+bradleyess@users.noreply.github.com>
2022-01-09 22:23:26 +1100

    wip: add audio scripts/functions

--- functions/audio.sh
+++ functions/audio.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-exifGetTitleFromTags()  {
+exifGetTitleFromTags() {
     echo exiftool -j "$1" | jq -r '.[].Album'
 }
 
-exifGetArtistFromTags()  {
+exifGetArtistFromTags() {
     echo exiftool -j "$1" | jq -r '.[].Artist'
 }
 
-exifGetAlbumFromTags()  {
+exifGetAlbumFromTags() {
     echo exiftool -j "$1" | jq -r '.[].Album'
 }
 
 buildTitleArtistAlbumObject() {
-    exiftool -j $1 | jq -r '.[] | { title: .Title, artist: .Artist, album: .Album }'
+    exiftool -j "$1" | jq -r '.[] | { title: .Title, artist: .Artist, album: .Album }'
 }
 
 getArtworkFromDiscogs() {
@@ -21,10 +21,17 @@ getArtworkFromDiscogs() {
     artist=$(cat $input | jq -r '.artist')
     album=$(cat $input | jq -r '.album')
 
-    curl $(
+    curl "$(
         curl --request GET \
-        --url "https://api.discogs.com/database/search?artist=$artist&release_title=$album" \
-        --header 'authorization: Discogs key=sOKrXQKkYUVXszuBShNN, secret=iOMqutIPnXhncAHaQnLcLKeNYSdajPBc' \
-        | jq -r '.results[0].cover_image'
-    ) > cover.jpg
+            --url "https://api.discogs.com/database/search?artist=$artist&release_title=$album" \
+            --header 'authorization: Discogs key=sOKrXQKkYUVXszuBShNN, secret=iOMqutIPnXhncAHaQnLcLKeNYSdajPBc' |
+            jq -r '.results[0].cover_image'
+    )" >cover.jpg
+}
+
+convertToAiff() {
+    file="$1"
+    destination="$2"
+
+    xld -o "$HOME/$destination" $file -f aif
 }
