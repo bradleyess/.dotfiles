cf573c0cdccfde3099f6abd2476ba1abf2c7f4c3
Bradley Shawyer <21098019+bradleyess@users.noreply.github.com>
2022-01-10 11:23:00 +1100

    aliases: wrap terminate instance function to prevent word splitting

--- aliases/aws.sh
+++ aliases/aws.sh
@@ -7,5 +7,5 @@ getEC2InstanceIdsByName() {
 
 terminateEC2ById() {
     instanceName=$1
-    aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --filters "Name=tag:Name,Values=$instanceName" --output text)
+    aws ec2 terminate-instances --instance-ids "$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --filters "Name=tag:Name,Values=$instanceName" --output text)"
 }
